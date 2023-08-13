f6e003087501521951bf481d529179f33d08160a
luongbs01 <54256832+luongbs01@users.noreply.github.com>
2023-08-13 09:50:19 +0700

    User Registration

---
+++ src/main/java/com/example/springredditclone/repository/VerificationTokenRepository.java
@@ -0,0 +1,7 @@
+package com.example.springredditclone.repository;
+
+import com.example.springredditclone.model.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+}

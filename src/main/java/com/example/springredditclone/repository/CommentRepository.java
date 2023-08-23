27de8ff3aa0e153c920795f0f1c57d50a7bd5aba
luongbs01 <54256832+luongbs01@users.noreply.github.com>
2023-08-23 19:44:23 +0700

    Comment API

--- src/main/java/com/example/springredditclone/repository/CommentRepository.java
+++ src/main/java/com/example/springredditclone/repository/CommentRepository.java
@@ -2,6 +2,7 @@ package com.example.springredditclone.repository;
 
 import com.example.springredditclone.model.Comment;
 import com.example.springredditclone.model.Post;
+import com.example.springredditclone.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,6 @@ import java.util.List;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByPost(Post post);
+
+    List<Comment> findAllByUser(User user);
 }

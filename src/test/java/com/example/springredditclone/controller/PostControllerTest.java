3a8c6adc2fcbd77247df54a2aa3096b401e7a58a
luongbs01 <54256832+luongbs01@users.noreply.github.com>
2023-08-25 13:09:03 +0700

    Test REST controller

---
+++ src/test/java/com/example/springredditclone/controller/PostControllerTest.java
@@ -0,0 +1,55 @@
+package com.example.springredditclone.controller;
+
+import com.example.springredditclone.dto.PostResponse;
+import com.example.springredditclone.service.PostService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static java.util.Arrays.asList;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@WebMvcTest(controllers = PostController.class)
+public class PostControllerTest {
+
+    @MockBean
+    private PostService postService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    @DisplayName("Should List All Posts When making GET request to endpoint - /api/posts/")
+    @WithMockUser(username = "test")
+    public void shouldGetAllPosts() throws Exception {
+        PostResponse postRequest1 = new PostResponse(1L, "Post Name", "http://url.site", "Description", "User 1",
+                "Subreddit Name", 0, 0, "1 day ago", false, false);
+        PostResponse postRequest2 = new PostResponse(2L, "Post Name 2", "http://url2.site2", "Description2", "User 2",
+                "Subreddit Name 2", 0, 0, "2 days ago", false, false);
+
+        Mockito.when(postService.getAllPosts()).thenReturn(asList(postRequest1, postRequest2));
+
+        // url must be the same as the one declared in post controller (no trailing slash), otherwise test will fail
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/posts"))
+                .andExpect(MockMvcResultMatchers.status().is(200))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.size()", Matchers.is(2)))
+                .andExpect(jsonPath("$[0].id", Matchers.is(1)))
+                .andExpect(jsonPath("$[0].postName", Matchers.is("Post Name")))
+                .andExpect(jsonPath("$[0].url", Matchers.is("http://url.site")))
+                .andExpect(jsonPath("$[1].url", Matchers.is("http://url2.site2")))
+                .andExpect(jsonPath("$[1].postName", Matchers.is("Post Name 2")))
+                .andExpect(jsonPath("$[1].id", Matchers.is(2)));
+    }
+}
